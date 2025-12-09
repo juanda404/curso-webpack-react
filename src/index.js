fd3a244ae65433f8a81982e0116aa5cf65f9161a
Juan David Santamaría <juanda404@gmail.com>
2025-12-09 06:52:31 -0500

    Configurando css y sass en react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { createRoot } from 'react-dom/client';
+import './styles/global.scss';
 
 //ReactDOM.render(<App />, document.getElementById('app'));
 
